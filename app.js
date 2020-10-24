6d08cc22c53a3d95a9e701b6a6b00706fae9a5ad
Amulya <computermaster97@gmail.com>
2020-10-25 02:51:36 +0530

    Added icons folder

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app=express();
 var PORT=process.env.PORT || 3000;
 
 app.use('/Plots',express.static('Plots'));
-
+app.use('/icons',express.static('icons'));
 app.listen(PORT,function(){
 
  console.log("server running on port 3000");
